b9c412f81b9daddafa551fe24c8bf92fe36987f1
Kyle Nicholson <kyleanicholson@gmail.com>
2023-12-04 21:40:33 -0800

    Improve data presentation for workouts, exercises, sets.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,48 @@
-import Link from 'next/link';
-import {prisma} from '@/db'
-import {WorkoutItem} from "@/components/WorkoutItem";
+import Link from "next/link";
+import { prisma } from "@/db";
+import { WorkoutItem } from "@/components/WorkoutItem";
 
 function getWorkouts() {
-    return prisma.workout.findMany()
-}
-
-async function getUsers() {
-    let users = await prisma.user.findMany()
-    let workouts =
-        // Include the user's workouts
-
-        console.log(users)
-
+  return prisma.workout.findMany({
+    include: {
+      exercises: {
+        include: { sets: true },
+      },
+    },
+  });
 }
 
 export default async function Home() {
-    getUsers()
-    const workouts = await getWorkouts()
-    // @ts-ignore
-    return <>
-        <header className="flex justify-between items-center mb-4">
-            <h1 className="text-2xl">Workouts</h1>
-            <nav className="flex justify-between gap-2">
-                <Link className=
-                          "border border-blue-500 rounded-md px-4 py-2 text-blue-500 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out"
-                      href="./new"> New Workout </Link>
-                <Link
-                    className="border border-blue-500 rounded-md px-4 py-2 text-blue-500 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out"
-                    href="./register"> Register</Link>
-            </nav>
-        </header>
-        <ul className="pl-4">
-            {workouts.map((workout) => (
-                <WorkoutItem key={workout.id} {...workout}
-                />
-            ))}
-        </ul>
+  // await resetWorkouts();
+  const workouts = await getWorkouts();
+  // @ts-ignore
+
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Workouts</h1>
+        <nav className="flex justify-between gap-2">
+          <Link
+            className="border border-blue-500 rounded-md px-4 py-2 text-blue-500 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out"
+            href="./new"
+          >
+            {" "}
+            New Workout{" "}
+          </Link>
+          <Link
+            className="border border-blue-500 rounded-md px-4 py-2 text-blue-500 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out"
+            href="./register"
+          >
+            {" "}
+            Register
+          </Link>
+        </nav>
+      </header>
+      <ul className="pl-4">
+        {workouts.map((workout) => (
+          <WorkoutItem key={workout.id} {...workout} />
+        ))}
+      </ul>
     </>
+  );
 }
--- src/components/WorkoutItem.tsx
+++ src/components/WorkoutItem.tsx
@@ -1,16 +1,69 @@
-type Exercise = {
-    title: string
-}
 type WorkoutItemProps = {
-    title: string,
-    description: string | null,
+  title: string;
+  workoutType: string;
+  exercises?: Exercise[];
+};
+type Exercise = {
+  id: string;
+  title: string;
+  duration?: number;
+  sets: Set[];
+  description?: string;
+};
+type Set = {
+  id: string;
+  reps: number;
+  weight: number;
+  rpe?: number;
+  rest?: number;
 };
 
-export function WorkoutItem({ title, description }:WorkoutItemProps){
-    return <li className="flex gap-1 item-center">
-        <div className="flex flex-col">
-            <h2 className="text-xl">{title}</h2>
-            <p className="text-gray-500">{description}</p>
-        </div>
+export function WorkoutItem({
+  title,
+  workoutType,
+  exercises = [],
+}: WorkoutItemProps) {
+  return (
+    <li className="flex gap-1 item-center">
+      <div className="flex flex-col">
+        <h2 className="text-xl">{title}</h2>
+        <p className="text-gray-500">{workoutType}</p>
+        <ul className="py-2">
+          {exercises.map((exercise) => (
+            <li key={exercise.id}>
+              {exercise.title}
+              {workoutType === "Resistance" && exercise.sets && (
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Weight</th>
+                      <th>Reps</th>
+                      <th>RPE</th>
+                      <th>Rest</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {exercise.sets.map((set, index) => (
+                      <tr key={index}>
+                        <td>{set.weight}</td>
+                        <td>{set.reps}</td>
+                        <td>{set.rpe}</td>
+                        <td>{set.rest}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )}
+              {workoutType === "Cardio" && <p>Duration: {exercise.duration}</p>}
+              {workoutType !== "Cardio" &&
+                workoutType !== "Resistance" &&
+                exercise.description && (
+                  <p>Description: {exercise.description}</p>
+                )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </li>
+  );
 }
